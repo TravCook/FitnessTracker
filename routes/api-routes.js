a697e299afd6f523a4da52edd87972983ea5b5bc
Travis Cook <travismcook93@gmail.com>
2020-11-18 15:17:55 -0700

    html routes are all working correctly...... i think

---
+++ routes/api-routes.js
@@ -0,0 +1,22 @@
+const db = require('../models')
+const path = require('path');
+const express = require('express')
+const app = express()
+
+app.get("/api/workouts",(req, res) => {
+  
+})
+
+app.post("/api/workouts", (req, res) => {
+
+})
+
+app.put("/api/workouts/:id", (req, res) => {
+
+})
+
+app.get("/api/workouts/range", (req,res) => {
+
+})
+
+module.exports = (app)
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+const path = require("path")
+const PORT = process.env.PORT || 8080
+const db = require("./models")
+const app = express();
+app.use(logger("dev"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static("public"));
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
@@ -7,3 +19,10 @@ mongoose.connect(
     useFindAndModify: false
   }
 );
+
+app.use(require("./routes/api-routes.js"))
+app.use(require("./routes/html-routes.js"))
+
+app.listen(PORT, () => {
+  console.log("App Running on " + PORT)
+})

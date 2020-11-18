08df0002976740ba5f74b6e07d68a7047fbb5fdb
Travis Cook <travismcook93@gmail.com>
2020-11-18 15:38:20 -0700

    api routes working???

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,15 +11,28 @@ app.get("/api/workouts",(req, res) => {
 })
 
 app.post("/api/workouts", (req, res) => {
-
+  db.Workout.create({}, (err, data) => {
+    if (err) throw err;
+    res.json(data)
+  })
 })
 
 app.put("/api/workouts/:id", (req, res) => {
-
+  db.Workout.findOneAndUpdate(
+    {_id: req.params.id},
+    { $push: {exercises: req.body}},
+    {new: true}, (err, data) => {
+      if(err) throw err;
+      res.json(data)
+    }
+  )
 })
 
 app.get("/api/workouts/range", (req,res) => {
-
+ db.Workout.find({}, (err, data) => {
+   if(err) throw err;
+   res.json(data)
+ })
 })
 
 module.exports = (app)
